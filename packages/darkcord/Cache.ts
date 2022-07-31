92102ece2b9ec97eb4a9259d7abb01871d6e0d4e
JustAWaifuHunter <hcsbrian4056@gmail.com>
2022-07-31 17:47:52 -0300

    feat(Cache): Added cache limit ✨

--- packages/darkcord/Cache.ts
+++ packages/darkcord/Cache.ts
@@ -1,1 +1,22 @@
-export class Cache<V = unknown> extends Map<string, V> {}
+export class Cache<V = unknown> extends Map<string, V> {
+  #limit: number
+  constructor (limit: number) {
+    super()
+
+    this.#limit = limit
+  }
+  set (key: string, value: V) {
+    super.set(key, value)
+
+    if (super.size >= this.#limit) {
+      this.#removeToLimit()
+    }
+
+    return this
+  }
+  #removeToLimit () {
+    while (super.size >= this.#limit) {
+      super.delete(super.keys().next().value)
+    }
+  }
+}
