dbeff298829658461b6b6d0a173dee6980d1cc17
JustAWaifuHunter <hcsbrian4056@gmail.com>
2022-08-04 12:24:17 -0300

    feat: base client 🦜

--- packages/darkcord/client/BaseClient.ts
+++ packages/darkcord/client/BaseClient.ts
@@ -1,5 +1,29 @@
-import {EventEmitter} from "deno/events"
+import {Rest} from "darkcord/rest"
+import EventEmitter from "deno/events"
 import {ClientEvents} from "../Events.ts"
+import {CacheFactory, CacheFactoryOptions} from "../utils/CacheFactory.ts"
 
-export abstract class BaseClient extends EventEmitter<Record<keyof ClientEvents, ClientEvents[keyof ClientEvents]>> {
+export interface ClientCache<Options = false> {
+    factory: Options extends true ? CacheFactoryOptions : CacheFactory
+}
+
+export abstract class BaseClient extends EventEmitter {
+  rest: Rest
+  cache: ClientCache
+  protected constructor (cache: ClientCache<true>) {
+    super()
+
+    this.rest = new Rest()
+    if (cache.factory instanceof CacheFactory) {
+      this.cache = cache as unknown as ClientCache<false>
+    } else {
+      this.cache = {
+        factory: new CacheFactory(cache.factory as CacheFactoryOptions, this)
+      }
+    }
+  }
+  on<E extends keyof ClientEvents> (event: E, listener: (...args: ClientEvents[E]) => unknown) {
+    super.on(event, listener)
+    return this
+  }
 }
