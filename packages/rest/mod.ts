56afc014f6a9797041c6bd1bbd2a53927dda15fe
Snow <73443391+JustAWaifuHunter@users.noreply.github.com>
2022-08-04 13:14:28 -0300

    chore: prefix complete with _

--- packages/rest/mod.ts
+++ packages/rest/mod.ts
@@ -5,9 +5,9 @@ export * from "./Rest.ts"
 export * from "./SequentialBucket.ts"
 
 export abstract class RestAction<T = unknown> {
-  #complete: (value: unknown) => Promise<T>
+  _complete: (value: unknown) => Promise<T>
   constructor (complete: (value: unknown) => Promise<T>) {
-    this.#complete = complete
+    this._complete = complete
   }
   abstract complete(important?: boolean): Promise<T>
   abstract queue(important?: boolean): Promise<void>
