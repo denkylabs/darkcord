e589122492b51f7d53b667dd2a5a9436e8d71a4e
Snow <73443391+JustAWaifuHunter@users.noreply.github.com>
2022-08-04 12:58:02 -0300

    feat: utils

---
+++ packages/darkcord/structures/User.ts
@@ -0,0 +1,38 @@
+import {Base} from "./Base.ts"
+import {APIUser, UserFlags as Flags, UserPremiumType, LocaleString} from "discord-api-types/v10"
+
+export class UserFlags {
+  constructor (public rawFlags: Flags) { }
+  has (flags: Flags) {
+    return (flags & this.rawFlags) === flags
+  }
+  static Flags = Flags
+}
+export class User extends Base {
+  readonly username: string
+  readonly discriminator: string
+  readonly avatar: string|null
+  readonly isBot?: boolean
+  readonly isSystem?: boolean
+  readonly mfaEnabled?: boolean
+  readonly banner?: string|null
+  readonly accentColor?: number|null
+  readonly locale?: LocaleString
+  readonly nitroType?: UserPremiumType
+  readonly publicFlags?: UserFlags
+  constructor (data: APIUser) {
+    super(data.id)
+
+    this.username = data.username
+    this.discriminator = data.discriminator
+    this.avatar = data.avatar
+    this.isBot = data.bot
+    this.isSystem = data.system
+    this.mfaEnabled = data.mfa_enabled
+    this.banner = data.banner
+    this.accentColor = data.accent_color
+    this.locale = data.locale as LocaleString
+    this.publicFlags = new UserFlags(Number(data.public_flags))
+    this.nitroType = data.premium_type
+  }
+}
--- packages/darkcord/utils/Options.ts
+++ packages/darkcord/utils/Options.ts
@@ -1,7 +1,28 @@
 import {ClientOptions} from "../client/ClientBuilder.ts"
+import {DefaultIntents} from "./Utils.ts"
 
-export function parseOptions (options: ClientOptions) {
-  return {
-    ...options
+export function verifyOptions (options?: ClientOptions) {
+  if (!options) {
+    return {
+      intents: DefaultIntents
+    }
   }
+
+  const types = {
+    intents: "number",
+    type: "number",
+    cacheFactory: "object"
+  }
+
+  for (const key of Object.keys(options)) {
+    const received = typeof options[key as keyof ClientOptions]
+    if (received !== types[key as keyof typeof types]) {
+      throw new TypeError(
+        `The ${key} must be a ${
+          types[key as keyof typeof types]
+        }, but received ${received}`
+      )
+    }
+  }
+  return options
 }
