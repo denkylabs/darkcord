4eb031f5a8169d34cd20aa84da1286d5eeae9050
JustAWaifuHunter <hcsbrian4056@gmail.com>
2022-08-04 12:20:36 -0300

    feat: guild cache 🍨

--- packages/darkcord/cache/GuildCache.ts
+++ packages/darkcord/cache/GuildCache.ts
@@ -1,14 +1,4 @@
-import {Cache} from "../Cache.ts"
+import {Cache} from "./Cache.ts"
 import {Guild} from "../structures/Guild.ts"
 
-export class GuildCache extends Cache<Guild> {
-  add (guild: Guild, replace = false) {
-    const id = guild.id.toString()
-    if (super.has(id) && !replace) {
-      return super.get(id)
-    }
-
-    super.set(id, guild)
-    return guild
-  }
-}
+export class GuildCache extends Cache<Guild> {}
